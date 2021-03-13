c4ebbc991e73f1eb9b8204e55ae8775e944ac8b8
Ritvik Sakpal <47916596+ritviksakpal@users.noreply.github.com>
2021-03-14 01:08:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PMS</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div style="display: flex;align-items: center;justify-content: center;height: 100vh;" class="container">
+        <a href="./Signup/AUTH/index.php">Login Page</a>
+    </div>
+</body>
+
+</html>
